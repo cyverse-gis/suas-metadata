5b09a7bc3755ecc91c634864bc0cf76ab7a4d087
DavidM1A2 <DavidM1A2@users.noreply.github.com>
2018-05-24 13:11:46 -0700

    Further performance improvements to the metadata reading

--- metadataReader.py
+++ metadataReader.py
@@ -2,9 +2,6 @@ import exiftool
 import json
 import os
 
-# Exif tool instance used to read image metadata
-exiftoolInstance = exiftool.ExifTool()
-
 # Map of actual EXIF metadata tags to our index metadata tags
 indexedFieldNames = \
 {
@@ -31,34 +28,10 @@ def main():
         text_file.write(jsonOut)
 
 def createJSONIndexForDirectory(directory):
-    # Start the exif tool (used for bulk optimization)
-    exiftoolInstance.start()
     # Create the index
-    indexedFiles = recursivelyCreateIndexEntries(directory)
-    # Terminate the exif tool
-    exiftoolInstance.terminate()
-    # Return the results
-    return json.dumps(indexedFiles)
-
-
-def recursivelyCreateIndexEntries(currentDirectory, indexedFiles = []):
-    # Index all image files in the directory and add them to the current list of indexed files
-    indexedFiles = indexedFiles + createIndexEntriesForDir(currentDirectory)
-    # Iterate over all directories (not files) in the current directory
-    for nextDirectory in listFiles(currentDirectory, includeDirectories=True, includeFiles=False):
-        # Compute the full path of the subdirectory
-        nextDirectoryPath = os.path.join(currentDirectory, nextDirectory)
-        # Make sure we can read the directory
-        indexedFiles = recursivelyCreateIndexEntries(nextDirectoryPath, indexedFiles)
-    return indexedFiles
-
-def createIndexEntriesForDir(directory):
-    # Raw files names is a list of files in a directory that need to be indexed. They are without path
-    rawFileNames = listFiles(directory, includeDirectories=False, includeFiles=True)
-    # Convert this list of raw files to a list of files with path appended. Also ensure all files are readable
-    filesToIndex = list(map(lambda rawFileName: os.path.join(directory, rawFileName), rawFileNames))
+    filesToIndex = getIndexableFiles(directory)
     # A list of indexed files in the form of a dictionary
-    indexedFiles = createIndexEntryForFiles(filesToIndex)
+    indexedFiles = createIndexEntriesForFiles(filesToIndex)
 
     # Function that testes a file to see if there's at least one non-null field
     def fileValid(indexedFile):
@@ -69,15 +42,48 @@ def createIndexEntriesForDir(directory):
 
     # Only keep files that actually have non-null fields. Indexing documents with all null fields is useless
     validIndexedFiles = list(filter(lambda indexedFile: fileValid(indexedFile), indexedFiles))
-    return validIndexedFiles
 
-def createIndexEntryForFiles(files):
+    # Return the results
+    return json.dumps(indexedFiles)
+
+def getIndexableFiles(currentDirectory, filesToIndex = []):
+    # Raw files names is a list of files in a directory that need to be indexed. They are without path
+    rawFileNamesInDir = listFiles(currentDirectory, includeDirectories=False, includeFiles=True)
+    # Index all image files in the directory and add them to the current list of indexed files
+    filesToIndex = filesToIndex + list(map(lambda rawFileName: os.path.join(currentDirectory, rawFileName), rawFileNamesInDir))
+    # Iterate over all directories (not files) in the current directory
+    for nextDirectory in listFiles(currentDirectory, includeDirectories=True, includeFiles=False):
+        # Compute the full path of the subdirectory
+        nextDirectoryPath = os.path.join(currentDirectory, nextDirectory)
+        # Make sure we can read the directory
+        filesToIndex = getIndexableFiles(nextDirectoryPath, filesToIndex)
+    return filesToIndex
+
+def listFiles(directory, includeDirectories=True, includeFiles=True):
+    # Return all files in a directory including sub directories if specified and including files if specified
+    try:
+        return [file for file in os.listdir(directory) if
+                os.access(os.path.join(directory, file), os.R_OK)
+                and
+                    ((includeDirectories and os.path.isdir(os.path.join(directory, file))
+                     or
+                    (includeFiles        and os.path.isfile(os.path.join(directory, file)))))]
+    except PermissionError:
+        return []
+
+def createIndexEntriesForFiles(files):
     # Ensure we dont have 0 files in a directory which crashes ExifTool
     if len(files) == 0:
         return []
 
+    # Exif tool instance used to read image metadata
+    exiftoolInstance = exiftool.ExifTool()
+    # Start the exif tool (used for bulk optimization)
+    exiftoolInstance.start()
     # Pull a list of tags off of a list of files
     exifInfoList = exiftoolInstance.get_tags_batch(indexedFieldNames, files)
+    # Terminate the exif tool
+    exiftoolInstance.terminate()
 
     # Remove any entries from the exif info list that we don't need by recreating it
     indexedInfoList = []
@@ -92,18 +98,5 @@ def createIndexEntryForFiles(files):
         indexedInfoList.append(indexedInfo)
 
     return indexedInfoList
-
-def listFiles(directory, includeDirectories=True, includeFiles=True):
-    # Return all files in a directory including sub directories if specified and including files if specified
-    try:
-        return [file for file in os.listdir(directory) if
-                os.access(os.path.join(directory, file), os.R_OK)
-                and
-                    ((includeDirectories and os.path.isdir(os.path.join(directory, file))
-                     or
-                    (includeFiles       and os.path.isfile(os.path.join(directory, file)))))]
-    except PermissionError:
-        return []
-
 
 main()
