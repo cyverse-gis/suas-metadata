d2ddfbc710ab2a64d8cecb920f5f0b124b5cab3b
DavidM1A2 <davidslovikosky@email.arizona.edu>
2018-06-19 13:50:36 -0700

    Commenting

--- Java Metadata Processor/src/main/java/DroneLogger.java
+++ Java Metadata Processor/src/main/java/DroneLogger.java
@@ -1,14 +1,27 @@
+/**
+ * Class used to wrap any debug or error messages with a boolean
+ */
 public class DroneLogger
 {
 	private static final Boolean DEBUG_MSGS = true;
 	private static final Boolean ERROR_MSGS = true;
 
+	/**
+	 * Logs an error messages if error messages are enabled
+	 *
+	 * @param error The error to print out
+	 */
 	public static void logError(String error)
 	{
 		if (ERROR_MSGS)
 			System.err.println(error);
 	}
 
+	/**
+	 * Logs an debug messages if debug messages are enabled
+	 *
+	 * @param debug The debug message to print out
+	 */
 	public static void logDebug(String debug)
 	{
 		if (DEBUG_MSGS)
