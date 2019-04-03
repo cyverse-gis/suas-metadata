b4fd2472b254d3626dba88b2c7bf5297cc49de81
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2019-04-03 09:27:49 -0700

    Update and rename ElasticSearch installation commands.txt to es_installation.sh

---
+++ CalliopeAuth/es_installation.sh
@@ -0,0 +1,12 @@
+/bin/bash 
+# Download elasticsearch image
+docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
+
+# Download kibana image:
+docker pull docker.elastic.co/kibana/kibana-oss:6.2.4
+
+# Run & Install ElasticSearch:
+docker run --restart always -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4
+
+# Run & Install kibana:
+docker run --restart always -d --net=host -e "ELASTICSEARCH_URL=http://localhost:9200" docker.elastic.co/kibana/kibana-oss:6.2.4
